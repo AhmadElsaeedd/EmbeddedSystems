a584fec20c553a9668354021a9b4c30fb92304f8
Ahmed Elsaeed <ae2200@nyu.edu>
2023-09-07 17:36:33 +0400

    code ready

--- Lab 1/Lab 1/Lab 1/main.cpp
+++ Lab 1/Lab 1/Lab 1/main.cpp
@@ -28,18 +28,39 @@ int main(void)
 	//C = 6th bit
 	//D = 5th bit
 	//E = 4th bit
-	//F = 5th bit
-	//G = 6th bit
+	//F = 3rd bit
+	//G = 2nd bit
+	//1st bit is always 0
 	uint8_t seven_segment_digits[10]={
 	0b11111100,//0
 	0b01100000,//1
-	0b11011001,//2
-	0b11110001,//3
+	0b11011010,//2
+	0b11110010,//3
 	0b01001100,//4
+	0b10110110,//5
+	0b10111110,//6
+	0b11100000,//7
+	0b11111110,//8
+	0b11110110,//9
 	};
+	
+	uint8_t current_digit = 0;
     while (1) 
     {
-	
+		//increment button is PB3
+		if (!(PINB & (1 << PINB3))) //button is active low because of pull-up
+		{
+			current_digit = (current_digit + 1) % 10; //after 9 is 0
+			PORTD = seven_segment_digits[current_digit]; //change the display
+		}
+		
+		//decrement button is PB4
+		if (!(PINB & (1 << PINB4))){//button is active low because of pull-up
+			if(current_digit == 0) current_digit = 9;
+			else current_digit--;
+			
+			PORTD = seven_segment_digits[current_digit]; //change the display
+		}
     }
 }
 
