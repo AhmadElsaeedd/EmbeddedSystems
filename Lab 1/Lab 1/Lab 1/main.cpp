5d16f0e6097980c86149c9d70a1eeb62a707754f
Ahmed Elsaeed <ae2200@nyu.edu>
2023-09-11 10:09:28 +0400

    testing segments

--- Lab 1/Lab 1/Lab 1/main.cpp
+++ Lab 1/Lab 1/Lab 1/main.cpp
@@ -10,17 +10,17 @@
 
 int main(void)
 {
+	//set PortB2 to input
+	DDRB &= ~(1<<DDB2);
 	//set PortB3 to input
-	DDRB &= ~(1<<DDB3);
-	//set PortB4 to input
-	DDRB &= ~(1 << DDB4);
+	DDRB &= ~(1 << DDB3);
 	//set all of PortD to output to control the 7-segment
 	DDRD = 0xFF;
 	
-	//enabling the pull-up resistor to PB3 and PB4
+	//enabling the pull-up resistor to PB2 and PB3
 	//this is done because we're connecting a button to those pins
+	PORTB |= (1 << PORTB2);
 	PORTB |= (1 << PORTB3);
-	PORTB |= (1 << PORTB4);
     
 	//Define the array that maps that contains the numbers
 	//A = 8th bit
@@ -32,6 +32,7 @@ int main(void)
 	//G = 2nd bit
 	//1st bit is always 0
 	uint8_t seven_segment_digits[10]={
+	//0b11111111,//test	
 	0b11111100,//0
 	0b01100000,//1
 	0b11011010,//2
@@ -48,19 +49,19 @@ int main(void)
     while (1) 
     {
 		//increment button is PB3
-		if (!(PINB & (1 << PINB3))) //button is active low because of pull-up
-		{
-			current_digit = (current_digit + 1) % 10; //after 9 is 0
+		//if (!(PINB & (1 << PINB2))) //button is active low because of pull-up
+		//{
+			//current_digit = (current_digit + 1) % 10; //after 9 is 0
 			PORTD = seven_segment_digits[current_digit]; //change the display
-		}
+		//}
 		
 		//decrement button is PB4
-		if (!(PINB & (1 << PINB4))){//button is active low because of pull-up
-			if(current_digit == 0) current_digit = 9;
-			else current_digit--;
+		//if (!(PINB & (1 << PINB3))){//button is active low because of pull-up
+			//if(current_digit == 0) current_digit = 9;
+			//else current_digit--;
 			
-			PORTD = seven_segment_digits[current_digit]; //change the display
-		}
+			//PORTD = seven_segment_digits[current_digit]; //change the display
+		//}
     }
 }
 
